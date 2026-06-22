f8f1ccb8a653046ef0a4974e32716bb865207fa5
jiayiwang00 <476525168@qq.com>
2026-06-23 00:00:42 +1000

    Update GitHub Pages path after repository rename

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import pluginChecker from "vite-plugin-checker";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-    base: "/-Flappy-Birb/",
+    base: "/Flappy-Bird/",
     plugins: [pluginChecker({ typescript: true, overlay: false })],
 });
